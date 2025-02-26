3f697e04626c95a1e52185620dcba22f8602f1cd
JDA <jadames14@gmail.com>
2025-02-26 11:45:53 -0500

    install expressJS, create expressApp.js
    demonstrates how expressJs makes creating middleware easier than using the core features from nodejs

---
+++ assignmentOne/expressApp.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+const express = require('express');
+
+const app = express(); 
+
+app.use((req, res, next) => {
+    console.log('in the middleware');
+    next(); //allows the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+    console.log('in the second middleware');
+    res.send('<h1>Hello</h1>')
+});
+
+const server = http.createServer(app);
+
+server.listen(3000);
